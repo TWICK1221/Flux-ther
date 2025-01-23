a8d9a76bc41139d417cd061b56c03d11bb883c4b
TWICK <74471738+TWICK1221@users.noreply.github.com>
2025-01-23 05:31:43 +0300

    Add files via upload

--- client/src/components/CustomInputMask.js
+++ client/src/components/CustomInputMask.js
@@ -1,1 +1,19 @@
+// Путь: src/components/CustomInputMask.js
+import React from 'react';
+import InputMask from 'react-input-mask-next';
 
+const CustomInputMask = ({ value, onChange, mask, placeholder }) => {
+    return (
+        <InputMask
+            mask={mask}
+            value={value}
+            onChange={onChange}
+            placeholder={placeholder}
+            maskChar="_" // Символ-заполнитель для пустых полей
+        >
+            {(inputProps) => <input {...inputProps} />}
+        </InputMask>
+    );
+};
+
+export default CustomInputMask;
