acd00dd231f08c505d5283828335673cbb7b5e68
TWICK <74471738+TWICK1221@users.noreply.github.com>
2025-01-24 06:01:41 +0300

    Add files via upload

---
+++ server/Data/MasterDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FluxÆther.Data
+{
+    public class MasterDbContext : DbContext
+    {
+        public MasterDbContext(DbContextOptions<MasterDbContext> options) : base(options) { }
+
+        public DbSet<UserDatabase> UserDatabases { get; set; }
+    }
+
+    public class UserDatabase
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string DatabaseName { get; set; }
+        public string ConnectionString { get; set; }
+    }
+}
---
+++ server/Data/MasterDbContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace FluxÆther.Data
+{
+    public class MasterDbContextFactory : IDesignTimeDbContextFactory<MasterDbContext>
+    {
+        public MasterDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<MasterDbContext>();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new MasterDbContext(optionsBuilder.Options);
+        }
+    }
+}
