3f6a7f70a102a34d095c6f8a7524d826728d914a
TWICK <74471738+TWICK1221@users.noreply.github.com>
2025-01-24 06:03:41 +0300

    Add files via upload

--- server/Migrations/MasterDb/20250124013721_CreateUserDatabaseTable.cs
+++ server/Migrations/MasterDb/20250124013721_CreateUserDatabaseTable.cs
@@ -1,1 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
 
+#nullable disable
+
+namespace FluxÆther.Migrations.MasterDb
+{
+    /// <inheritdoc />
+    public partial class CreateUserDatabaseTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserDatabases",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    DatabaseName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ConnectionString = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserDatabases", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserDatabases");
+        }
+    }
+}
