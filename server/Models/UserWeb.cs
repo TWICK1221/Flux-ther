74ff91d3922997737344f167426b7dc1f7c45ab9
TWICK <74471738+TWICK1221@users.noreply.github.com>
2025-01-24 03:58:21 +0300

    Add files via upload

---
+++ server/Models/UserWeb.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRMsystem.Models
+{
+    public class UserWeb
+    {
+        [Key]
+        public int Id { get; set; } // Уникальный идентификатор пользователя
+
+        [Required]
+        [StringLength(100)]
+        public string Username { get; set; } // Имя пользователя (логин)
+
+        [Required]
+        public string PasswordHash { get; set; } // Хэш пароля
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow; // Дата создания пользователя
+    }
+}
