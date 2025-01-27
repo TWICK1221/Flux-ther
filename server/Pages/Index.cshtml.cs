e1788c80a80d4ab4738faf172ad7853e610b7690
TWICK <74471738+TWICK1221@users.noreply.github.com>
2025-01-27 14:53:53 +0300

    Add files via upload

--- server/Pages/Index.cshtml.cs
+++ server/Pages/Index.cshtml.cs
@@ -1,7 +1,7 @@
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace CRMsystem.Pages
+namespace FluxÆther.Pages
 {
     public class IndexModel : PageModel
     {
